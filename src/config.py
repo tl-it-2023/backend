4d7c9ec1d07227192f09c50d9dcf2aee9f03b26f
begenFys <nikita.perminov.work@gmail.com>
2023-08-10 05:14:46 +0500

    Add funcs in config.py

--- src/config.py
+++ src/config.py
@@ -15,3 +15,14 @@ DB_PASS = os.environ.get("DB_PASS")
 STORAGE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "storage")
 STORAGE_RESUME_FILE = os.path.join(STORAGE_PATH, "unsort")
 STORAGE_RESUME = os.path.join(STORAGE_PATH, "resume")
+
+
+def folders_create():
+    folders = [STORAGE_PATH, STORAGE_RESUME_FILE, STORAGE_RESUME]
+    for folder in folders:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+
+if __name__ == "__main__":
+    folders_create()
